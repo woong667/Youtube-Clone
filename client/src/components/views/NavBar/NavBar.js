91db9b809ad931c4617f93cd528c5b4f94614dcc
woong667 <woong667@naver.com>
2020-08-08 18:22:26 +0900

    최종

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import LeftMenu from './Sections/LeftMenu';
 import RightMenu from './Sections/RightMenu';
 import { Drawer, Button, Icon } from 'antd';
 import './Sections/Navbar.css';
+import { YoutubeOutlined } from '@ant-design/icons';
 
 
 
@@ -19,8 +20,8 @@ function NavBar() {
 
   return (
     <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
-      <div className="menu__logo">
-        <a href="/">Kuntube</a>
+      <div style={{width:'10%' ,height:'40px'}} className="menu__logo">
+        <a href="/"><YoutubeOutlined style={{ width: '100%',fontSize: '40px', color: 'red' }}/><span style={{color:'red',marginLeft:'20px'}}>kuntube</span></a>
       </div>
       <div className="menu__container">
         <div className="menu_left">
